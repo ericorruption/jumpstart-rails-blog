974b33e9c5c2af83cabeb4798492ab8ce63c34c5
Eric Quanz <eric.quanz@gmail.com>
2016-04-23 15:02:24 -0300

    list post tags on post view, and show a list of articles for each tag on tag#show

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
+
+  resources :tags
+
   root 'articles#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
