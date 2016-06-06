ad814df800295e9be7b20b34953ba387462c929d
Eric Quanz <eric.quanz@gmail.com>
2016-06-05 23:07:05 -0300

    update require_login method message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def require_login
-    redirect_to(login_path, notice: "You need to do this.") unless current_user
+    redirect_to(login_path, notice: "You need to be logged in to do this.") unless current_user
   end
 end
